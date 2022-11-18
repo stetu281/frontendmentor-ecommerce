60ac746caac4bda69bd6ffd28a4d4e820baf78d8
Stefan Turner <turner.stefan281@gmail.com>
2022-11-18 09:25:39 +0100

    add mobile nav functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,11 @@
 import css from "../scss/main.scss";
+
+const nav = document.querySelector(".nav");
+
+document.querySelector(".header__nav-btn").addEventListener("click", () => {
+  nav.classList.add("nav--open");
+});
+
+document.querySelector(".nav__close-btn").addEventListener("click", () => {
+  nav.classList.remove("nav--open");
+});
