b8afa13f31c134cef97594ad44e66d449ce60915
Stefan Turner <turner.stefan281@gmail.com>
2022-11-19 08:42:22 +0100

    overlay slider working

--- src/js/slider.js
+++ src/js/slider.js
@@ -11,21 +11,66 @@ export const slider = () => {
         if (i == imgNum - 1) {
           imgContainer.dataset.counter = 0;
           imgContainer.style.transform = `translateX(0%)`;
+
+          document.querySelectorAll(".slider__thumb-hover").forEach((item) => {
+            item.dataset.activethumb = "false";
+            if (item.children[0].dataset.thumbid == 0) {
+              item.dataset.activethumb = "true";
+            }
+          });
         } else {
           i++;
           imgContainer.dataset.counter = i;
           imgContainer.style.transform = `translateX(-${i * 100}%)`;
+
+          document.querySelectorAll(".slider__thumb-hover").forEach((item) => {
+            item.dataset.activethumb = "false";
+            console.log(item.children);
+            if (item.children[0].dataset.thumbid == i) {
+              item.dataset.activethumb = "true";
+            }
+          });
         }
       } else {
         if (i == 0) {
           imgContainer.dataset.counter = imgNum - 1;
           imgContainer.style.transform = `translateX(-${(imgNum - 1) * 100}%)`;
+
+          document.querySelectorAll(".slider__thumb-hover").forEach((item) => {
+            item.dataset.activethumb = "false";
+            if (item.children[0].dataset.thumbid == 3) {
+              item.dataset.activethumb = "true";
+            }
+          });
         } else {
           i--;
           imgContainer.dataset.counter = i;
           imgContainer.style.transform = `translateX(-${i * 100}%)`;
+
+          document.querySelectorAll(".slider__thumb-hover").forEach((item) => {
+            item.dataset.activethumb = "false";
+            if (item.children[0].dataset.thumbid == i) {
+              item.dataset.activethumb = "true";
+            }
+          });
         }
       }
     });
   });
+
+  document
+    .querySelector(".slider__thumbs-container")
+    .addEventListener("click", (e) => {
+      document.querySelectorAll(".slider__thumb-hover").forEach((item) => {
+        item.dataset.activethumb = "false";
+      });
+
+      if (e.target && e.target.matches("img")) {
+        e.target.parentElement.dataset.activethumb = "true";
+        imgContainer.dataset.counter = e.target.dataset.thumbid;
+        imgContainer.style.transform = `translateX(-${
+          e.target.dataset.thumbid * 100
+        }%)`;
+      }
+    });
 };
