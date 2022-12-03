45b59945b26d42a580f1880d8150fbe33cfd6510
Stefan Turner <stefanturner@MacBook-Air-von-Stefan.local>
2022-12-03 05:30:39 +0100

    change output folder name

--- webpack.dev.js
+++ webpack.dev.js
@@ -9,7 +9,7 @@ module.exports = merge(common, {
   },
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "docs"),
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
